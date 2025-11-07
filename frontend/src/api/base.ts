597426c2c0951fabc33f4c2d30b83849059a2de1
manh <manh99294@donga.edu.vn>
2025-11-08 00:07:43 +0700

    fix ngnix update api

--- frontend/src/api/base.ts
+++ frontend/src/api/base.ts
@@ -1,6 +1,4 @@
-const LOCAL_API_URL = import.meta.env.VITE_APP_API_URL;
-
-const API_BASE_URL = LOCAL_API_URL || "";
+const API_BASE_URL ="";
 
 export async function apiFetch(endpoint: string, options: RequestInit = {}) {
   const fullUrl = `${API_BASE_URL}${endpoint.startsWith('/') ? endpoint : '/' + endpoint}`;
